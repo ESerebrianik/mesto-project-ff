7b75cac3c6559ef1c6b835be0d80b654f548af11
Elena Serebrianik <e.serebryanik@yandex.ru>
2024-05-31 22:56:56 +0100

    Version0.1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
-// @todo: Темплейт карточки
-
-// @todo: DOM узлы
-
 // @todo: Функция создания карточки
+function createCard(data) {
+    dataCard = cardTemplate.querySelector('.card').cloneNode(true);
+    dataCard.querySelector('.card__image').src = data.link;
+    dataCard.querySelector('.card__title').textContent = data.name;
+    dataCard.querySelector('.card__image').alt = data.name;
+    dataCard.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+    return dataCard;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(evt) {
+    evt.target.closest('.card').remove();
+}
+
+// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+
+// @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
+
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function(dataCard){
+   cardElement = createCard(dataCard);
+   placesList.append(cardElement); 
+});
+
+
+
