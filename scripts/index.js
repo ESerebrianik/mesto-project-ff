7c4ec62725ed1a8f23bfd1d225f83d2a1b077c19
Elena Serebrianik <e.serebryanik@yandex.ru>
2024-06-01 14:05:22 +0100

    Version0.2 исправленная по комментариям ревьюера

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,22 @@
-// @todo: Функция создания карточки
-function createCard(data) {
-    dataCard = cardTemplate.querySelector('.card').cloneNode(true);
-    dataCard.querySelector('.card__image').src = data.link;
-    dataCard.querySelector('.card__title').textContent = data.name;
-    dataCard.querySelector('.card__image').alt = data.name;
-    dataCard.querySelector('.card__delete-button').addEventListener('click', deleteCard);
-    return dataCard;
-}
-
-// @todo: Функция удаления карточки
-function deleteCard(evt) {
-    evt.target.closest('.card').remove();
-}
-
-// @todo: Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content;
+const cardsContainer = document.querySelector('.places__list');
 
-// @todo: DOM узлы
-const placesList = document.querySelector('.places__list');
+function createCard(data, onDelete) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    const cardImage = cardElement.querySelector('.card__image');
+    cardImage.src = data.link;
+    cardImage.alt = data.name;
+    cardElement.querySelector('.card__title').textContent = data.name;
+    deleteButton.addEventListener('click', () => onDelete(cardElement));
+    return cardElement;
+}
 
+function deleteCard(card) {
+    card.remove();
+}
 
-// @todo: Вывести карточки на страницу
 initialCards.forEach(function(dataCard){
-   cardElement = createCard(dataCard);
-   placesList.append(cardElement); 
+   let card = createCard(dataCard, deleteCard);
+   cardsContainer.append(card); 
 });
-
-
-
